fb7865e50aa8b10df47508523c759367ab219fac
Rodrigo Haruo Noguti <rodrigonoguti@gmail.com>
2021-02-25 09:45:02 -0300

    First commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,20 @@
+import '../styles/global.css';
+
+import { ChallengesProvider } from '../contexts/ChallengesContext';
+import { useState } from 'react';
+
+function MyApp({ Component, pageProps }) {
+  const [level, setLevel] = useState(1);
+
+  function levelUp() {
+    setLevel(level + 1);
+  }
+
+  return (
+    <ChallengesProvider>
+      <Component {...pageProps} />
+    </ChallengesProvider>
+  );
+}
+
+export default MyApp
